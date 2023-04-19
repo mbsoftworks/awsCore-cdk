992425564e81d628b700870848197cef26908d87
Markus Bergkvist <mbergkvist@users.noreply.github.com>
2023-04-19 14:44:44 +0200

    Export functions from context module at top level

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,10 @@
 export * as constants from './constants';
 export * as context from './context';
 export * as pipeline from './pipeline';
+
+export {
+  createResourceName,
+  formatResourceName,
+  getEnvironmentAbbreviation,
+  getProjectName,
+} from './context';
